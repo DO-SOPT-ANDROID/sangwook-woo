6434139af0563cd18d45481ef785ed1851409159
Sangwook123 <operawook@catholic.ac.kr>
2023-10-09 20:59:43 +0900

    [ui] 로그인 뷰 디자인

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.sopt.dosopttemplate.presentation.login
+
+import android.os.Bundle
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.ActivityLoginBinding
+import org.sopt.dosopttemplate.util.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
