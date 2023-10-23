d5239e99a4042cfd6687c0607c49bcd6bb248e08
Sangwook123 <operawook@catholic.ac.kr>
2023-10-23 14:20:54 +0900

    [feat] 프래그먼트

---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/doandroid/DoAndroidFragment.kt
@@ -0,0 +1,10 @@
+package org.sopt.dosopttemplate.presentation.main.doandroid
+
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.FragmentDoandroidBinding
+import org.sopt.dosopttemplate.util.binding.BindingFragment
+
+@AndroidEntryPoint
+class DoAndroidFragment : BindingFragment<FragmentDoandroidBinding>(R.layout.fragment_doandroid) {
+}
---
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeFragment.kt
@@ -0,0 +1,10 @@
+package org.sopt.dosopttemplate.presentation.main.home
+
+import dagger.hilt.android.AndroidEntryPoint
+import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.databinding.FragmentHomeBinding
+import org.sopt.dosopttemplate.util.binding.BindingFragment
+
+@AndroidEntryPoint
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+}
