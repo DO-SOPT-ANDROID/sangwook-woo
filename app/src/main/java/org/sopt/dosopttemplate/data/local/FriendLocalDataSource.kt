cdce226c83d7074beb53114455324f98778f0dbc
Sangwook123 <operawook@catholic.ac.kr>
2023-10-27 14:33:47 +0900

    [feat] datasource 인터페이스

---
+++ app/src/main/java/org/sopt/dosopttemplate/data/local/FriendLocalDataSource.kt
@@ -0,0 +1,11 @@
+package org.sopt.dosopttemplate.data.local
+
+import org.sopt.dosopttemplate.data.model.local.FriendInfoEntity
+
+interface FriendLocalDataSource {
+    suspend fun getAll(): List<FriendInfoEntity>
+    suspend fun addFriend(friendInfoEntity: FriendInfoEntity)
+    suspend fun addFriends(friendList: List<FriendInfoEntity>)
+    suspend fun deleteFriend(friendInfoEntity: FriendInfoEntity)
+    suspend fun deleteFriendById(id: Int)
+}
