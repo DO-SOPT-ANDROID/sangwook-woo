e26fad142b4eb9016dbed320ebb207878437cf72
Sangwook123 <operawook@catholic.ac.kr>
2023-10-27 14:37:48 +0900

    [add] binding adapter coil

--- app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
@@ -1,15 +1,41 @@
 package org.sopt.dosopttemplate.util.binding
 
 import android.view.View
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import coil.load
+import coil.transform.RoundedCornersTransformation
+import org.sopt.dosopttemplate.R
 
 @BindingAdapter("Music")
 fun TextView.Music(music: String?) {
-    if(music == null){
+    if (music == null) {
         visibility = View.GONE
-    }else{
+    } else {
         text = music
         visibility = View.VISIBLE
+    }
+}
+
+@BindingAdapter("Music")
+fun LinearLayout.Music(music: String?) {
+    if (music == null) {
+        visibility = View.GONE
+    } else {
+        visibility = View.VISIBLE
+    }
+}
+
+@BindingAdapter("setImageUrl")
+fun ImageView.setImageUrl(imageUrl: String?) {
+    if (imageUrl == null) {
+        load(R.drawable.img_profile)
+    } else {
+        load(imageUrl) {
+            placeholder(R.drawable.img_profile)
+            transformations(RoundedCornersTransformation(10F))
+        }
     }
 }
