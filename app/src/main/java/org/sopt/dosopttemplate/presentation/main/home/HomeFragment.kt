1c9bf4bfe6d054c817fbeff1256a92b2e3c11009
Sangwook123 <operawook@catholic.ac.kr>
2023-10-27 14:40:18 +0900

    [feat] homeFragment

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeFragment.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeFragment.kt
@@ -10,24 +10,54 @@ import org.sopt.dosopttemplate.presentation.model.HomeModel
 import org.sopt.dosopttemplate.presentation.model.UserModel
 import org.sopt.dosopttemplate.util.binding.BindingFragment
 import org.sopt.dosopttemplate.util.intent.getParcelable
+import org.sopt.dosopttemplate.util.view.UiState
 
 @AndroidEntryPoint
 class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
     private val viewModel by viewModels<HomeViewModel>()
+    lateinit var myInfo: HomeModel.MyInfoModel
+    lateinit var homeAdapter: HomeAdapter
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initHomeRecyclerViewAdapter()
+        initHomeAdapter()
+        initFriendListStateObserver()
+        getMyInfo()
+        getFriendList()
     }
 
-    private fun initHomeRecyclerViewAdapter() {
-        val userModel = requireActivity().intent.getParcelable(USER_KEY, UserModel::class.java)
-        val myInfo = HomeModel.MyInfoModel(userModel?.nickname, userModel?.discription)
-        val adapter = HomeAdapter()
-        val list = listOf(myInfo) + viewModel.friendInfoList.sortedBy { it.name }
-        adapter.submitList(list)
-        binding.rvHome.adapter = adapter
+    private fun initHomeAdapter() {
+        homeAdapter = HomeAdapter()
+        binding.rvHome.adapter = homeAdapter
     }
+
+    private fun initFriendListStateObserver() {
+        viewModel.friendListState.observe(viewLifecycleOwner) { state ->
+            when (state) {
+                is UiState.Success -> {
+                    submitHomeList(state)
+                }
+
+                else -> {}
+            }
+        }
+    }
+
+    private fun submitHomeList(state: UiState.Success<List<HomeModel.FriendInfoModel>>) {
+        val list = listOf(myInfo) + state.data.sortedBy { it.name }
+        homeAdapter.submitList(list)
+    }
+
+    private fun getMyInfo() {
+        val userModel = requireActivity().intent.getParcelable(USER_KEY, UserModel::class.java)
+        val myInformation = HomeModel.MyInfoModel(0, userModel?.nickname, userModel?.discription)
+        myInfo = myInformation
+    }
+
+    private fun getFriendList() {
+        viewModel.getFriendList()
+    }
+
     companion object {
         private const val USER_KEY = "user"
     }
