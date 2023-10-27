275cc69b1d10ea9057f0249cee9d14b396696d7b
Sangwook123 <operawook@catholic.ac.kr>
2023-10-27 14:37:28 +0900

    [feat] friend mapping fun

--- app/src/main/java/org/sopt/dosopttemplate/presentation/model/HomeModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/model/HomeModel.kt
@@ -1,10 +1,13 @@
 package org.sopt.dosopttemplate.presentation.model
 
-import androidx.annotation.DrawableRes
+import org.sopt.dosopttemplate.domain.entity.Friend
 import java.time.LocalDate
 
 sealed class HomeModel {
+    abstract val id: Int?
+
     data class MyInfoModel(
+        override val id: Int?,
         val name: String?,
         val discription: String?,
     ) : HomeModel() {
@@ -14,15 +17,35 @@ sealed class HomeModel {
     }
 
     data class FriendInfoModel(
-        val id: Int,
+        override val id: Int?,
         val name: String,
-        val birthday: LocalDate,
+        val birthday: LocalDate?,
         val music: String?,
-        @DrawableRes val image: Int? = null,
+        val imageUri: String?,
     ) : HomeModel() {
         companion object {
             const val NORMAL_FRIEND_VIEW_TYPE = 1
             const val BIRTHDAY_FRIEND_VIEW_TYPE = 2
+
+            fun toFriendInfoModel(friendList: List<Friend>) = friendList.map { data ->
+                FriendInfoModel(
+                    id = data.id,
+                    name = data.name,
+                    birthday = data.birthday,
+                    music = data.music,
+                    imageUri = data.imageUri
+                )
+            }
+
+            fun toFriend(friendInfoList: List<FriendInfoModel>) = friendInfoList.map { data ->
+                Friend(
+                    id = data.id,
+                    name = data.name,
+                    birthday = data.birthday,
+                    music = data.music,
+                    imageUri = data.imageUri
+                )
+            }
         }
     }
 }
