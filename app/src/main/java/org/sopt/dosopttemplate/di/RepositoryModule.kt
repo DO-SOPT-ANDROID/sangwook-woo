d42434f8a10ddddaec6ef234b506e39dc123d615
Sangwook123 <operawook@catholic.ac.kr>
2023-10-27 14:35:29 +0900

    [feat] repository di

--- app/src/main/java/org/sopt/dosopttemplate/di/RepositoryModule.kt
+++ app/src/main/java/org/sopt/dosopttemplate/di/RepositoryModule.kt
@@ -4,7 +4,9 @@ import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import org.sopt.dosopttemplate.data.repository.FriendLocalRepositoryImpl
 import org.sopt.dosopttemplate.data.repository.SharedPrefRepositoryImpl
+import org.sopt.dosopttemplate.domain.repository.FriendLocalRepository
 import org.sopt.dosopttemplate.domain.repository.SharedPrefRepository
 import javax.inject.Singleton
 
@@ -16,4 +18,10 @@ abstract class RepositoryModule {
     abstract fun bindsSharedPrefRepository(
         sharedPrefRepository: SharedPrefRepositoryImpl
     ): SharedPrefRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindsFriendLocalRepository(
+        friendLocalRepository: FriendLocalRepositoryImpl
+    ): FriendLocalRepository
 }
