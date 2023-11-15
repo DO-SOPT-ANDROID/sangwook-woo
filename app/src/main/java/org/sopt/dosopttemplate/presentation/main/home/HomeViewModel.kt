c9d992f808afb8e282e63026378d8877e5c6d5c7
Sangwook123 <operawook@catholic.ac.kr>
2023-11-16 01:42:03 +0900

    [feat] getuserlist

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeViewModel.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeViewModel.kt
@@ -1,48 +1,51 @@
 package org.sopt.dosopttemplate.presentation.main.home
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import org.sopt.dosopttemplate.domain.repository.FriendLocalRepository
-import org.sopt.dosopttemplate.presentation.model.HomeModel
-import org.sopt.dosopttemplate.presentation.model.HomeModel.FriendInfoModel.Companion.toFriendInfoModel
+import org.sopt.dosopttemplate.domain.entity.ReqresUser
+import org.sopt.dosopttemplate.domain.repository.UserRepository
 import org.sopt.dosopttemplate.util.view.UiState
+import retrofit2.HttpException
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val friendLocalRepository: FriendLocalRepository
+    private val userRepository: UserRepository
 ) : ViewModel() {
-    private val _friendListState =
-        MutableStateFlow<UiState<List<HomeModel.FriendInfoModel>>>(UiState.Empty)
-    val friendListState: StateFlow<UiState<List<HomeModel.FriendInfoModel>>> get() = _friendListState
-
     private val _friendDeleteState = MutableStateFlow<UiState<Any>>(UiState.Empty)
-    val friendDeleteState: StateFlow<UiState<Any>> get() = _friendDeleteState
+    val friendDeleteState: StateFlow<UiState<Any>> = _friendDeleteState.asStateFlow()
 
-    fun getFriendList() {
-        viewModelScope.launch {
-            friendLocalRepository.getAll()
-                .onSuccess { response ->
-                    _friendListState.value = UiState.Success(toFriendInfoModel(response))
-                }
-                .onFailure { t ->
-                    _friendListState.value = UiState.Failure("${t.message}")
-                }
-        }
+    private val _userListState =
+        MutableStateFlow<UiState<PagingData<ReqresUser>>>(UiState.Empty)
+    val userListState: StateFlow<UiState<PagingData<ReqresUser>>> = _userListState.asStateFlow()
+
+    init {
+        getUsers()
     }
 
-    fun deleteUser(id: Int?) {
+    fun getUsers() {
         viewModelScope.launch {
-            if (id != null) {
-                friendLocalRepository.deleteFriendById(id)
-                _friendDeleteState.value = UiState.Success(true)
-            } else {
-                _friendDeleteState.value = UiState.Failure(ID_NULL_ERROR)
-            }
+            _userListState.emit(UiState.Loading)
+            userRepository.getUser()
+                .cachedIn(viewModelScope)
+                .catch { t ->
+                    if (t is HttpException) {
+                        Log.e("서버통신", "Http오류 ! ${t.message}, ${t.code()}")
+                    }
+                }
+                .collectLatest { data ->
+                    _userListState.emit(UiState.Success(data))
+                }
         }
     }
 
