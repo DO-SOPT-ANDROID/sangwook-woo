1a770a9f9bbca4dae71692bf57128e13d8d7bbaa
Sangwook123 <operawook@catholic.ac.kr>
2023-11-16 01:11:12 +0900

    [feat] retrofit qualifier

---
+++ app/src/main/java/org/sopt/dosopttemplate/di/qualifier/RetrofitQualifier.kt
@@ -0,0 +1,11 @@
+package org.sopt.dosopttemplate.di.qualifier
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class AUTH
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class REQRES
