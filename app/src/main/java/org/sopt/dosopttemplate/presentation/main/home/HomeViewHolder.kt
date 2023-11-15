7bc35f42b168a05890afc68d1a1d50a8f28cd84c
Sangwook123 <operawook@catholic.ac.kr>
2023-11-16 01:39:12 +0900

    [feat] paging viewholder

--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeViewHolder.kt
@@ -1,54 +1,24 @@
 package org.sopt.dosopttemplate.presentation.main.home
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.dosopttemplate.databinding.ItemHomeBirthdayBinding
-import org.sopt.dosopttemplate.databinding.ItemHomeHeaderBinding
 import org.sopt.dosopttemplate.databinding.ItemHomePostBinding
-import org.sopt.dosopttemplate.presentation.model.HomeModel
+import org.sopt.dosopttemplate.domain.entity.ReqresUser
 
-sealed class HomeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    class MyInfoViewHolder(
-        private val binding: ItemHomeHeaderBinding
-    ) : HomeViewHolder(binding.root) {
-        fun onBind(data: HomeModel.MyInfoModel) {
-            binding.data = data
+class HomeViewHolder(
+    private val binding: ItemHomePostBinding,
+    private val onLongClicked: (ReqresUser) -> Unit,
+    private val onClicked: (ReqresUser) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(data: ReqresUser?) {
+        binding.data = data
+        if (data == null) return
+
+        binding.root.setOnLongClickListener {
+            onLongClicked(data)
+            return@setOnLongClickListener true
         }
-    }
-
-    class FriendViewHolder(
-        private val binding: ItemHomePostBinding,
-        private val onLongClicked: (HomeModel.FriendInfoModel) -> Unit,
-        private val onClicked: (HomeModel.FriendInfoModel) -> Unit
-    ) : HomeViewHolder(binding.root) {
-        fun onBind(data: HomeModel.FriendInfoModel) {
-            binding.data = data
-            binding.root.setOnLongClickListener {
-                onLongClicked(data)
-                return@setOnLongClickListener true
-            }
-            binding.root.setOnClickListener {
-                onClicked(data)
-            }
-            binding.executePendingBindings()
-        }
-    }
-
-    class BirthdayViewHolder(
-        private val binding: ItemHomeBirthdayBinding,
-        private val onLongClicked: (HomeModel.FriendInfoModel) -> Unit,
-        private val onClicked: (HomeModel.FriendInfoModel) -> Unit
-    ) : HomeViewHolder(binding.root) {
-        fun onBind(data: HomeModel.FriendInfoModel) {
-            binding.data = data
-            binding.root.setOnLongClickListener {
-                onLongClicked(data)
-                return@setOnLongClickListener true
-            }
-            binding.root.setOnClickListener {
-                onClicked(data)
-            }
-            binding.executePendingBindings()
+        binding.root.setOnClickListener {
+            onClicked(data)
         }
     }
 }
--- app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeVpViewHolder.kt
+++ app/src/main/java/org/sopt/dosopttemplate/presentation/main/home/HomeVpViewHolder.kt
@@ -1,34 +1,24 @@
 package org.sopt.dosopttemplate.presentation.main.home
 
-import android.view.View
 import androidx.recyclerview.widget.RecyclerView
-import org.sopt.dosopttemplate.databinding.ItemHomeVpHeaderBinding
 import org.sopt.dosopttemplate.databinding.ItemHomeVpPostBinding
-import org.sopt.dosopttemplate.presentation.model.HomeModel
+import org.sopt.dosopttemplate.domain.entity.ReqresUser
 
-sealed class HomeVpViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    class MyInfoViewHolder(
-        private val binding: ItemHomeVpHeaderBinding
-    ) : HomeVpViewHolder(binding.root) {
-        fun onBind(data: HomeModel.MyInfoModel) {
-            binding.data = data
+class HomeVpViewHolder (
+    private val binding: ItemHomeVpPostBinding,
+    private val onLongClicked: (ReqresUser) -> Unit,
+    private val onClicked: (ReqresUser) -> Unit
+) : RecyclerView.ViewHolder(binding.root) {
+    fun onBind(data: ReqresUser?) {
+        binding.data = data
+        if (data == null) return
+
+        binding.root.setOnLongClickListener {
+            onLongClicked(data)
+            return@setOnLongClickListener true
         }
-    }
-
-    class FriendViewHolder(
-        private val binding: ItemHomeVpPostBinding,
-        private val onLongClicked: (HomeModel.FriendInfoModel) -> Unit,
-        private val onClicked: (HomeModel.FriendInfoModel) -> Unit
-    ) : HomeVpViewHolder(binding.root) {
-        fun onBind(data: HomeModel.FriendInfoModel) {
-            binding.data = data
-            binding.root.setOnLongClickListener {
-                onLongClicked(data)
-                return@setOnLongClickListener true
-            }
-            binding.root.setOnClickListener {
-                onClicked(data)
-            }
+        binding.root.setOnClickListener {
+            onClicked(data)
         }
     }
 }
