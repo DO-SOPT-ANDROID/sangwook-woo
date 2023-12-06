d822de8229c36a1c74fb5bc1c361301114111eab
Sangwook123 <operawook@catholic.ac.kr>
2023-12-06 13:08:38 +0900

    [feat] binding adapter

--- app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
+++ app/src/main/java/org/sopt/dosopttemplate/util/binding/BindingAdapter.kt
@@ -1,13 +1,24 @@
 package org.sopt.dosopttemplate.util.binding
 
+import android.util.TypedValue
+import android.view.ContextThemeWrapper
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.content.res.AppCompatResources.getColorStateList
+import androidx.core.content.ContextCompat
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import androidx.databinding.BindingAdapter
+import androidx.databinding.InverseBindingAdapter
+import androidx.databinding.InverseBindingListener
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import org.sopt.dosopttemplate.R
+import org.sopt.dosopttemplate.domain.entity.ValidationResult
 import java.time.LocalDate
 
 @BindingAdapter("Music")
@@ -43,14 +54,94 @@ fun ImageView.setImageUrl(imageUrl: String?) {
 
 @BindingAdapter("birthDay")
 fun View.birthDay(birthday: LocalDate?) {
-    if(birthday?.monthValue == LocalDate.now().monthValue && birthday.dayOfMonth == LocalDate.now().dayOfMonth) {
+    if (birthday?.monthValue == LocalDate.now().monthValue && birthday.dayOfMonth == LocalDate.now().dayOfMonth) {
         visibility = View.VISIBLE
-    }else{
+    } else {
         visibility = View.GONE
     }
 }
 
 @BindingAdapter("setBirthday")
 fun TextView.setBirthday(birthday: LocalDate?) {
-    this.text = resources.getString(R.string.friend_detail_birthday,birthday?.monthValue,birthday?.dayOfMonth)
+    this.text = resources.getString(
+        R.string.friend_detail_birthday,
+        birthday?.monthValue,
+        birthday?.dayOfMonth
+    )
+}
+
+@BindingAdapter("validateId")
+fun TextView.validateId(validationResult: ValidationResult?) {
+    if (validationResult != null) {
+        this.text = validationResult.errorMessage
+        when (validationResult.successful) {
+            true -> {}
+            false -> {
+                this.setTextColor(ContextCompat.getColor(this.context, R.color.red050))
+            }
+        }
+    }
+}
+
+@BindingAdapter("validatePw")
+fun TextView.validatePw(validationResult: ValidationResult?) {
+    if (validationResult != null) {
+        this.text = validationResult.errorMessage
+        when (validationResult.successful) {
+            true -> {}
+            false -> {
+                this.setTextColor(ContextCompat.getColor(this.context, R.color.red050))
+            }
+        }
+    }
+}
+
+@BindingAdapter("validateSignup")
+fun Button.validateSignup(signupValidation: Boolean) {
+    val themedContext = ContextThemeWrapper(context, R.style.Theme_DoSoptTemplate)
+
+    val typedValue = TypedValue()
+    themedContext.theme.resolveAttribute(android.R.attr.colorPrimary, typedValue, true)
+    val colorPrimaryResId = typedValue.resourceId
+
+    val errorColor = R.color.gray050
+
+    val colorStateList = if (signupValidation) {
+        getColorStateList(context, colorPrimaryResId)
+    } else {
+        getColorStateList(context, errorColor)
+    }
+
+    this.backgroundTintList = colorStateList
+    this.isEnabled = signupValidation
+}
+
+@BindingAdapter("focus")
+fun EditText.setFocus(focus: Boolean) {
+    if (focus) {
+        this.requestFocus()
+    } else {
+        this.clearFocus()
+    }
+}
+
+@InverseBindingAdapter(attribute = "focus", event = "focusChange")
+fun EditText.getFocus(): Boolean {
+    return isFocused
+}
+
+@BindingAdapter("focusChange")
+fun EditText.focusChange(inverseBindingListener: InverseBindingListener?) {
+    this.setOnFocusChangeListener { _, hasFocus ->
+        inverseBindingListener?.onChange()
+    }
+}
+
+@BindingAdapter("validVisibility")
+fun TextView.validVisibility(focus: Boolean) {
+    if (focus) {
+        this.isVisible = true
+    } else {
+        this.isInvisible = true
+    }
 }
